5189a50828d4a5c6a69ce4a555113425d043baf8
v venkata ramana <vramana.159@gmail.com>
2026-02-04 20:19:37 +0530

    positive conduction

--- test.py
+++ test.py
@@ -1,14 +1,13 @@
-from flask import Flask
+num = int ( input('enter any number '))
 
-app = Flask('myapp')
-@app.route("/sendEmail")
-def sendEmailtoclient():
-    return "EMAIL send.. "
+if num  > 0 : # -87 > 0 False
 
-@app.route("/log/<mag>")
-def log(msg):
-    return 'info: your Log' + msg +"is recored" 
-@app.route("/showBlog/<int:postID>")
-def showblog(postID):
-    return 'showing Blog with Post ID' + str(postID)
-app.run(debug=True)
+  print('+ve')
+
+elif num == 0 :
+
+  print('neutral')
+
+else:
+
+  print('-ve')
