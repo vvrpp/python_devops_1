8ca8444560473fa002f56465a70728d73b1f2d2b
vvrpp <vramana.159@gmail.com>
2026-09-25 07:53:29 +0530

    add the commit for day4

---
+++ fun_1.py
@@ -0,0 +1,12 @@
+import fun_mod_pak as fmd
+
+'''num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+print(fmd.additio(num1, num2))
+'''
+
+fmd.subtraction()
+
+print(fmd.num1)
+
+print(fmd.num2)
